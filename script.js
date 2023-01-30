0da71104c8ca21765dc7527059cc436afe4f2196
codesoup <82330389+helikopterelidojosa@users.noreply.github.com>
2023-01-30 02:11:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,322 @@
+var chapterNR = 0;
+var textNR = 0;
+var randomizeText;
+function getRandomText(){
+    chapterNR = Math.ceil(Math.random()*18);
+
+    switch (chapterNR) {
+  case 1:
+    randomizeText = 46;
+    break;
+  case 2:
+    randomizeText = 72;
+    break;
+  case 3:
+     randomizeText = 43
+    break;
+  case 4:
+    randomizeText = 42;
+    break;
+  case 5:
+    randomizeText = 29;
+    break;
+  case 6:
+    randomizeText = 47;
+    break;
+
+ case 7:
+    randomizeText = 30;
+    break;
+      case 8:
+        randomizeText = 28;
+    break;
+  case 9:
+    randomizeText = 34;
+    break;
+  case 10:
+    randomizeText = 42;
+    break;
+  case 11:
+    randomizeText = 55;
+    break;
+  case 12:
+    randomizeText = 20;
+    break;
+  case 13:
+    randomizeText = 35;
+    break;
+  case 14:
+    randomizeText = 27;
+    break;
+    case 15:
+        randomizeText = 20;
+        break;
+      case 16:
+        randomizeText = 24;
+        break;
+      case 17:
+        randomizeText = 28;
+        break;
+      case 18:
+        randomizeText = 78;
+}
+textNR = Math.ceil(Math.random()*randomizeText)
+
+
+
+if(chapterNR == 1 && textNR == 16){
+    textNR = "16-18"
+}
+if(chapterNR == 1 && textNR == 17){
+    textNR = "16-18"
+}
+if(chapterNR == 1 && textNR == 17){
+    textNR = "16-18"
+}
+//
+if(chapterNR == 1 && textNR == 21){
+    textNR = "21-22"
+}
+if(chapterNR == 1 && textNR == 22){
+    textNR = "21-22"
+}
+//
+if(chapterNR == 1 && textNR == 32){
+    textNR = "32-35"
+}
+if(chapterNR == 1 && textNR == 33){
+    textNR = "32-35"
+}
+if(chapterNR == 1 && textNR == 34){
+    textNR = "32-35"
+}
+if(chapterNR == 1 && textNR == 35){
+    textNR = "32-35"
+}
+//
+if(chapterNR == 1 && textNR == 37){
+    textNR = "37-38"
+}
+if(chapterNR == 1 && textNR == 38){
+    textNR = "37-38"
+}
+//
+if(chapterNR == 2 && textNR == 42){
+    textNR = "42-43"
+}
+if(chapterNR == 2 && textNR == 43){
+    textNR = "42-43"
+}
+//
+if(chapterNR == 5 && textNR == 8){
+    textNR = "8-9"
+}
+if(chapterNR == 5 && textNR == 9){
+    textNR = "8-9"
+}
+//
+if(chapterNR == 5 && textNR == 27){
+    textNR = "27-28"
+}
+if(chapterNR == 5 && textNR ==28){
+    textNR = "27-28"
+}
+//
+if(chapterNR == 6 && textNR == 11){
+    textNR = "11-12"
+}
+if(chapterNR == 6 && textNR ==12){
+    textNR = "11-12"
+}
+//
+if(chapterNR == 6 && textNR == 13){
+    textNR = "13-14"
+}
+if(chapterNR == 6 && textNR ==14){
+    textNR = "13-14"
+}
+//
+if(chapterNR == 6 && textNR == 20){
+    textNR = "20-23"
+}
+if(chapterNR == 6 && textNR == 21){
+    textNR = "20-23"
+}
+if(chapterNR == 6 && textNR == 22){
+    textNR = "20-23"
+}
+if(chapterNR == 6 && textNR == 23){
+    textNR = "20-23"
+}
+//
+if(chapterNR == 10 && textNR == 4){
+    textNR = "4-5"
+}
+if(chapterNR == 10 && textNR == 5){
+    textNR = "4-5"
+}
+//
+if(chapterNR == 10 && textNR == 12){
+    textNR = "12-13"
+}
+if(chapterNR == 10 && textNR == 13){
+    textNR = "12-13"
+}
+//
+if(chapterNR == 11 && textNR == 10){
+    textNR = "10-11"
+}
+if(chapterNR == 11 && textNR == 11){
+    textNR = "10-11"
+}
+//
+if(chapterNR == 11 && textNR == 26){
+    textNR = "26-27"
+}
+if(chapterNR == 11 && textNR == 27){
+    textNR = "26-27"
+}
+//
+if(chapterNR == 11 && textNR == 41){
+    textNR = "41-42"
+}
+if(chapterNR == 11 && textNR == 42){
+    textNR = "41-42"
+}
+//
+if(chapterNR == 12 && textNR == 3){
+    textNR = "3-4"
+}
+if(chapterNR == 12 && textNR == 4){
+    textNR = "3-4"
+}
+//
+if(chapterNR == 12 && textNR == 6){
+    textNR = "6-7"
+}
+if(chapterNR == 12 && textNR == 7){
+    textNR = "6-7"
+}
+//
+if(chapterNR == 12 && textNR == 13){
+    textNR = "13-14"
+}
+if(chapterNR == 12 && textNR == 14){
+    textNR = "13-14"
+}
+//
+if(chapterNR == 12 && textNR == 18){
+    textNR = "18-19"
+}
+if(chapterNR == 12 && textNR == 19){
+    textNR = "18-19"
+}
+//
+if(chapterNR == 13 && textNR == 1){
+    textNR = "1-2"
+}
+if(chapterNR == 13 && textNR == 2){
+    textNR = "1-2"
+}
+//
+if(chapterNR == 13 && textNR == 6){
+    textNR = "6-7"
+}
+if(chapterNR == 13 && textNR == 7){
+    textNR = "6-7"
+}
+//
+if(chapterNR == 13 && textNR == 8){
+    textNR = "8-12"
+}
+if(chapterNR == 13 && textNR == 9){
+    textNR = "8-12"
+}
+if(chapterNR == 13 && textNR == 10){
+    textNR = "8-12"
+}
+if(chapterNR == 13 && textNR == 11){
+    textNR = "8-12"
+}
+if(chapterNR == 13 && textNR == 12){
+    textNR = "8-12"
+}
+//
+if(chapterNR == 14 && textNR == 22){
+    textNR = "22-25"
+}
+if(chapterNR == 14 && textNR == 23){
+    textNR = "22-25"
+}
+if(chapterNR == 14 && textNR == 24){
+    textNR = "22-25"
+}
+if(chapterNR == 14 && textNR == 25){
+    textNR = "22-25"
+}
+//
+if(chapterNR == 15 && textNR == 3){
+    textNR = "3-4"
+}
+if(chapterNR == 15 && textNR == 4){
+    textNR = "3-4"
+}
+//
+if(chapterNR == 16 && textNR == 1){
+    textNR = "1-3"
+}
+if(chapterNR == 16 && textNR == 3){
+    textNR = "1-3"
+}
+//
+if(chapterNR == 16 && textNR == 11){
+    textNR = "11-12"
+}
+if(chapterNR == 16 && textNR == 12){
+    textNR = "11-12"
+}
+//
+if(chapterNR == 16 && textNR == 13){
+    textNR = "13-15"
+}
+if(chapterNR == 16 && textNR == 14){
+    textNR = "13-15"
+}
+if(chapterNR == 16 && textNR == 15){
+    textNR = "13-15"
+}
+//
+if(chapterNR == 17 && textNR == 5){
+    textNR = "5-6"
+}
+if(chapterNR == 17 && textNR == 6){
+    textNR = "5-6"
+}
+//
+if(chapterNR == 17 && textNR == 26){
+    textNR = "26-27"
+}
+if(chapterNR == 17 && textNR == 27){
+    textNR = "26-27"
+}
+//
+if(chapterNR == 18 && textNR == 51){
+    textNR = "51-23"
+}
+if(chapterNR == 18 && textNR == 52){
+    textNR = "51-53"
+}
+if(chapterNR == 18 && textNR == 53){
+    textNR = "51-53"
+}
+//
+
+document.getElementById("please-read").href = "https://vedabase.io/en/library/bg/" + chapterNR + "/" + textNR + "/";
+document.getElementById("please-read").innerHTML = "BG. " + chapterNR + "." + textNR;
+
+document.getElementById("show-link").href = "https://vedabase.io/en/library/bg/" + chapterNR + "/" + textNR + "/";
+document.getElementById("show-link").innerHTML = "read online: https://vedabase.io/en/library/bg/" + chapterNR + "/" + textNR + "/";
+
+
+document.getElementById("show-result").style.display = "block";
+}
